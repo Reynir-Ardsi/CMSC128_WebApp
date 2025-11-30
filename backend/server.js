5ee989adadca86e942b446620fa405653829d469
Rainier RJ Espinal <rjespinal6404@gmail.com>
2025-12-01 02:03:35 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -293,5 +293,5 @@ apiRouter.delete("/tasks/completed", async (req, res) => {
     res.json({ message: "Cleared" });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
